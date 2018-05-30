3f9d1c7aec5a88a042c8988fd52010c5b085e5e1
SushmithaReddyThumma <37255775+SushmithaReddyThumma@users.noreply.github.com>
2018-05-30 15:38:02 -0500

    Error message

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# MyDreamApp
+# Angular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.0-rc.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.3.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthService } from './services/auth.service';
+import {RouterModule, Routes} from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+} from "angular5-social-login";
+import { HttpModule } from '@angular/http';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '', component: LoginComponent }
-];
 
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+      [{
+        id: FacebookLoginProvider.PROVIDER_ID,
+        provider: new FacebookLoginProvider("2070774416285250")
+      },
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider("50573442860-6lf1eme9lnsjra8vl8mil36qn2ecm959.apps.googleusercontent.com")
+        },
+      ]
+  )
+  return config;
+}
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent
+    SigninComponent
   ],
-  imports: [
-    BrowserModule, HttpModule, RouterModule.forRoot(routes), FormsModule
+  imports:[
+    BrowserModule,
+    AppRoutingModule,
+    SocialLoginModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path: 'signin', component: SigninComponent}
+    ])
   ],
-  providers: [AuthService],
+  providers: [
+  
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
